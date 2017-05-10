e4f8cff3f836a065b0f6d5a40b0cdf14630e11c6
Akshat Goel <akshatgo@gmail.com>
2017-05-10 18:29:33 +0530

    done

---
+++ app/src/main/java/com/filters/ControlActivity.java
@@ -0,0 +1,37 @@
+package com.filters;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ControlActivity extends AppCompatActivity {
+
+    Toolbar mControlToolbar;
+
+    ImageView mTickImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control);
+
+        mControlToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        mControlToolbar.setTitle(getString(R.string.app_name));
+        mControlToolbar.setNavigationIcon(R.mipmap.icon);
+        mControlToolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
+
+        mTickImageView = (ImageView) findViewById(R.id.imageView3);
+        mTickImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ControlActivity.this,ImagePreviewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
